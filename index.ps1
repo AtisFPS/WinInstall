e418d1f8b150fc8e5db3833862a5ee87a8c36b50
Atis <94861317+AtisFPS@users.noreply.github.com>
2024-09-05 17:20:09 +0200

    Add files via upload

--- index.ps1
+++ index.ps1
@@ -2,7 +2,10 @@
 $ProjectName = "WinSetup"
 $host.UI.RawUI.WindowTitle = "$ProjectName - Atis"
 
-$temp           = "$env:TEMP"
+$temp           = "$env:TEMP/"
+New-Item -Path "$temp/WinSetupByAtis" -ItemType Directory
+$temp           = "$env:TEMP/WinSetupByAtis"
+
 $setupURL      = "https://raw.githubusercontent.com/AtisFPS/WinSetup/main/setup"
 $cdnURL        = "https://raw.githubusercontent.com/AtisFPS/WinSetup/main/upload/"
 $ScriptsPath = "$scriptURL/"
@@ -11,14 +14,14 @@ Add-Type -AssemblyName PresentationFramework
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName 'System.Windows.Forms'
 Add-Type -AssemblyName 'System.Drawing'
-Import-Module -Name PowerShellGet
-Install-Module -Name WindowsDesktopBackground
+#Import-Module -Name PowerShellGet
+#Install-Module -Name WindowsDesktopBackground
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 
 function EchoToolsBox {
     Clear-Host
     Write-Host ''
-    Write-Host 'Tout telechargement sera effectue dans les dossier temporaire ou dans vos images '
+    Write-Host 'Tout les telechargements sont dans votre dossier %temp% ou votre dossier utilisateur   '
     Write-Host ''
     Write-Host '    $$\      $$\ $$\                   $$$$$$\             $$\                         '
     Write-Host '    $$ | $\  $$ |\__|                 $$  __$$\            $$ |                        '
@@ -47,68 +50,25 @@ function EchoToolsBox {
     Write-Host 'All right reserved'
     Write-Host '--'
 }
+function Install-Standard {
+$Standard = "./Install-Standard/index.ps1"
+Start-Process powershell.exe -ArgumentList "-NoProfile -WindowStyle Minimized -ExecutionPolicy Bypass -File `"$Standard`"" -Verb RunAs
+}
+function Install-Custom {
+$Custom = "./Install-Custom/index.ps1"
+Start-Process powershell.exe -ArgumentList "-NoProfile -WindowStyle Minimized -ExecutionPolicy Bypass -File `"$Custom`"" -Verb RunAs
+}
 
 function Winget-Setup {
     $winget = "./setup/winget.ps1"
     Start-Process powershell.exe -ArgumentList "-NoProfile -WindowStyle Minimized -ExecutionPolicy Bypass -File `"$winget`"" -Verb RunAs
 }
 
-function Graph-Install {
-    EchoToolsBox
-    #############################################################
-    #               Menu Principal                              #
-    #############################################################
-    
-    $form = New-Object System.Windows.Forms.Form
-    $form.Text = "$ProjectName - Menu Principal"
-    $form.Size = New-Object System.Drawing.Size(400, 650)
-    # Defini le logo comme icone de la fenetre
-    $form.Icon = [System.Drawing.Icon]::FromHandle((New-Object System.Drawing.Bitmap $LogoPath).GetHicon())
-    # Defini le fond d ecran de la fenetre
-    $form.BackgroundImage = [System.Drawing.Image]::FromFile($BackgroundPath)
-    $form.BackgroundImageLayout = "Stretch"
-    $form.StartPosition = "CenterScreen"  
-
-    ##########################################################################
-     #   Affiche le panneau de configuration
-     $buttonOutils = New-Object System.Windows.Forms.Button
-     $buttonOutils.Text = "Outils d'administration Windows "
-     $buttonOutils.Size = New-Object System.Drawing.Size(300, 30)
-     $buttonOutils.Location = New-Object System.Drawing.Point(50, 20)
-     $buttonOutils.Add_Click({
-         UtilsFunction
-     })
-     $form.Controls.Add($buttonOutils)
-    
-    ##########################################################################
-    # Creer un bouton "Quitter"
-    $buttonQuitter = New-Object System.Windows.Forms.Button
-    $buttonQuitter.Text = "Quitter"
-    $buttonQuitter.Size = New-Object System.Drawing.Size(300, 30)
-    $buttonQuitter.Location = New-Object System.Drawing.Point(50, 570)
-    $buttonQuitter.Add_Click({
-        $form.Close()
-    })
-
-    # Ajouter le bouton "Quitter" a la fenetre
-    $form.Controls.Add($buttonQuitter)
-    
-    ##########################################################################
-    
-    # Afficher la fenetre
-    $form.ShowDialog()
-}
 function Graph-Cache {
-    $LogoUrl = "$cdnURL/logo-tools.png"
-    $LogoPath = "$temp/logo-tools.png"
-    $BackgroundURL = "$cdnURL/background-tools.jpg"
-    $BackgroundPath = "$temp/background-tools.jpg"
-
- if (Test-Path $BannerSanctionPath) {
-    $buttonSanctionRapide.BackgroundImage = [System.Drawing.Image]::FromFile($BannerSanctionPath)
-} else {
-    Write-Host "Erreur: Le fichier $BannerSanctionPath n'existe pas."
-}
+    $LogoUrl = "$cdnURL/logo.png"
+    $LogoPath = "$temp/logo.png"
+    $BackgroundURL = "$cdnURL/wallpaper.jpg"
+    $BackgroundPath = "$temp/wallpaper.jpg"
 
     if (-not (Test-Path $BackgroundPath)) {
         $wc = New-Object System.Net.WebClient
@@ -127,6 +87,81 @@ function Graph-Cache {
         }
     }
 }
+
+function Graph-Install {
+    $LogoPath = "$temp/logo.png"
+    $BackgroundPath = "$temp/wallpaper.jpg"
+    #EchoToolsBox
+    #############################################################
+    #               Menu Principal                              #
+    #############################################################
+    
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = "$ProjectName - Menu Principal"
+    $form.Size = New-Object System.Drawing.Size(400, 650)
+    # Defini le logo comme icone de la fenetre
+    $form.Icon = [System.Drawing.Icon]::FromHandle((New-Object System.Drawing.Bitmap $LogoPath).GetHicon())
+    # Defini le fond d ecran de la fenetre
+    $form.BackgroundImage = [System.Drawing.Image]::FromFile($BackgroundPath)
+    $form.BackgroundImageLayout = "Stretch"
+    $form.StartPosition = "CenterScreen"  
+    $buttonEmplacement = "20"
+
+    ##########################################################################
+     #   Affiche le panneau de configuration
+     $buttonStandardInstall = New-Object System.Windows.Forms.Button
+     $buttonStandardInstall.Text = "Lancer l'auto-install"
+     $buttonStandardInstall.Size = New-Object System.Drawing.Size(300, 30)
+     $buttonStandardInstall.Location = New-Object System.Drawing.Point(50, $buttonEmplacement)
+     $buttonStandardInstall.Add_Click({
+         Install-Standard
+     })
+     $form.Controls.Add($buttonStandardInstall)
+    
+    ##########################################################################
+         #   Affiche le panneau de configuration
+     $buttonCustomInstall = New-Object System.Windows.Forms.Button
+     $buttonCustomInstall.Text = "Install custom"
+     $buttonCustomInstall.Size = New-Object System.Drawing.Size(300, 30)
+     $buttonCustomInstall.Location = New-Object System.Drawing.Point(50, "$boutonEmplacement+70")
+     $buttonCustomInstall.Add_Click({
+         Install-Custom
+     })
+     $form.Controls.Add($buttonCustomInstall)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ##########################################################################
+    # Creer un bouton "Quitter"
+    $buttonQuitter = New-Object System.Windows.Forms.Button
+    $buttonQuitter.Text = "Quitter"
+    $buttonQuitter.Size = New-Object System.Drawing.Size(300, 30)
+    $buttonQuitter.Location = New-Object System.Drawing.Point(50, 570)
+    $buttonQuitter.Add_Click({
+        $form.Close()
+    })
+
+    # Ajouter le bouton "Quitter" a la fenetre
+    $form.Controls.Add($buttonQuitter)
+    
+    ##########################################################################
+    
+    # Afficher la fenetre
+    $form.ShowDialog()
+}
+EchoToolsBox
+Graph-Cache
 Graph-Install
 EchoToolsBox
 #Winget-Setup
